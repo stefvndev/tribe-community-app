db765a9468c2601d9788631f49976727516c58a2
stefvndev <lol.delta123@gmail.com>
2024-12-20 18:47:13 +0100

    feat: Setup TanStack query and router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div className="text-red-600">Workifyy</div>;
-};
-
-export default App;
---
+++ src/routes/__root.tsx
@@ -0,0 +1,19 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="flex gap-2 p-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  ),
+});
