d0ebecbe22a353943cb913b818df8cf25eca285b
stefvndev <lol.delta123@gmail.com>
2024-12-24 20:33:15 +0100

    feat: setup shadcn

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,18 @@
 import { IconSelector } from "@tabler/icons-react";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
     <header className="absolute top-0 left-0 right-0 w-full h-16 px-4 bg-white border-b">
       <nav className="flex items-center justify-between w-full h-full mx-auto max-w-landing">
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
           <h2 className="text-4xl font-bold">tribu</h2>
-          <button type="button">
-            <IconSelector size={26} className="text-grayout" />
-          </button>
+          <Button size="icon" variant="ghost" className="rounded-full p-6">
+            <IconSelector
+              size={26}
+              className="text-grayout hover:text-black !size-7"
+            />
+          </Button>
         </div>
         <button
           type="button"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundColor: {
-        primary: "#F8F7F5",
-      },
-      maxWidth: {
-        landing: "1075px",
-      },
-      colors: {
-        grayout: "#909090",
-      },
-    },
+  	extend: {
+  		backgroundColor: {
+  			primary: '#F8F7F5'
+  		},
+  		maxWidth: {
+  			landing: '1075px'
+  		},
+  		colors: {
+  			grayout: '#909090'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
