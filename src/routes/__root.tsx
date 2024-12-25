28b7497a2f9ea82e376fa99404b93274648f01a3
stefvndev <lol.delta123@gmail.com>
2024-12-25 11:23:16 +0100

    feat: Disable tanstack on prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,24 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Navbar from "../components/navbar/Navbar";
+import React from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : React.lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
+
+const ReactQueryDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : React.lazy(() =>
+        import("@tanstack/react-query-devtools").then((res) => ({
+          default: res.ReactQueryDevtools,
+        }))
+      );
 
 export const Route = createRootRoute({
   component: () => (
@@ -10,6 +27,7 @@ export const Route = createRootRoute({
       <div className="pt-16">
         <Outlet />
       </div>
+      {/* devtools */}
       <TanStackRouterDevtools />
       <ReactQueryDevtools initialIsOpen={false} />
     </>
