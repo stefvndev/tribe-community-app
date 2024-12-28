fa51d8f65b9c68a12c8c4a86f4faf2309d48bc6b
stefvndev <lol.delta123@gmail.com>
2024-12-28 11:54:27 +0100

    feat: Connect Login to the Pocketbase

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,10 +1,56 @@
+import { useMemo } from "react";
+import PocketBase from "pocketbase";
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { toast } from "sonner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+
+type TSubmitData = {
+  email: string;
+  password: string;
+};
+
+const validationSchema = z.object({
+  email: z.string().min(1, "Email is required"),
+  password: z.string().min(1, "Password is required"),
+});
 
 export const Route = createFileRoute("/(auth)/login")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const pb = useMemo(
+    () => new PocketBase(import.meta.env.VITE_API_BASE_URL),
+    []
+  );
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TSubmitData>({
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onSubmit = async (data: TSubmitData) => {
+    const { email, password } = data;
+    try {
+      await pb.collection("users").authWithPassword(email, password);
+      toast("Login Successful!", {
+        description: "You have logged in successfully!",
+      });
+      console.log(pb.authStore);
+    } catch {
+      toast.error("Login Failed!", {
+        description: "Please check your credentials and try again.",
+      });
+    }
+  };
+
   return (
     <main className="flex items-center px-4 pb-6 min-h-[calc(100vh-64px)] justify-center w-full h-full">
       <div className="flex flex-col items-center p-8  max-sm:w-full max-sm:text-center w-[452px] h-full bg-white rounded-xl shadow-custom">
@@ -17,17 +63,44 @@ function RouteComponent() {
         <h2 className="mb-8 text-2xl font-bold text-dark-primary">
           Log in to Tribe
         </h2>
-        <form className="flex flex-col items-center w-full h-full gap-4">
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full h-[52px] border text-dark-primary font-base px-4 rounded-md"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full h-[52px] border text-dark-primary font-base px-4 rounded-md"
-          />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col items-center w-full h-full gap-4"
+        >
+          <label className="flex flex-col w-full gap-1">
+            <Input
+              {...register("email")}
+              type="email"
+              placeholder="Email"
+              className="w-full h-[52px] border text-dark-primary font-base px-4 rounded-md"
+              hasError={!!errors?.email?.message}
+            />
+            <p
+              className={cn(
+                !errors?.email?.message && "hidden",
+                "flex text-xs font-medium text-red-600"
+              )}
+            >
+              {errors?.email?.message}
+            </p>
+          </label>
+          <label className="flex flex-col w-full gap-1">
+            <Input
+              {...register("password")}
+              type="password"
+              placeholder="Password"
+              className="w-full h-[52px] border text-dark-primary font-base px-4 rounded-md"
+              hasError={!!errors?.password?.message}
+            />
+            <p
+              className={cn(
+                !errors?.password?.message && "hidden",
+                "flex text-xs font-medium text-red-600"
+              )}
+            >
+              {errors?.password?.message}
+            </p>
+          </label>
           <button
             type="submit"
             className="flex items-center justify-center w-full h-12 px-4 mt-2 font-bold uppercase rounded-md bg-yellow-primary text-dark-primary hover:bg-yellow-primary-hover"
