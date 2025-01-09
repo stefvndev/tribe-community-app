95ed005c024dbb883819f4abde2f4d61625d16a7
stefvndev <lol.delta123@gmail.com>
2025-01-09 15:43:13 +0100

    feat: remove userid from check

--- src/lib/useLoggedState.ts
+++ src/lib/useLoggedState.ts
@@ -1,9 +1,7 @@
 import { pb } from "@/api/pocketbase";
-import Cookies from "js-cookie";
 
 export const useLoggedState = () => {
-  const userId = Cookies.get("userId");
-  const isLogged = () => pb.authStore.isValid && !!userId;
+  const isLogged = () => pb.authStore.isValid;
 
   return { isLogged };
 };
